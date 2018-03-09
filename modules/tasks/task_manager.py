1e63175ac845b090de897a7309b70aa8bbebde49
niivek <niivek@gmail.com>
2018-03-08 21:39:17 -0800

    modified task

--- modules/tasks/task_manager.py
+++ modules/tasks/task_manager.py
@@ -1,7 +1,9 @@
 import rospy
 from random import *
+from std_msgs.msg import Int32MultiArray
 
-from modules.sensors.computer_vision import utils
+from modules.sensors.computer_vision import DetectBuoy
+from modules.sensors.computer_vision import DiceDetector
 from modules.control.navigation import Navigation
 
 
@@ -13,48 +15,76 @@ class TaskManager():
 
         self.coordinates = []
         self.navigation = Navigation()
+        rospy.init_node('task_manager')
         #self.is_killswitch_on = navigation.check_kill()
 
-    def gate_detect(self):
+    def detect_gate(self):
         """ When gate_detect task is called. """
 
     # TODO need to find out which cv node to get coordinates from
-        print("gate_detect")
+        print("detect_gate")
         self.coordinates.append(randint(-1,1))
         self.coordinates.append(randint(-1,1))
         self.navigation.nagivate(1, 1, 1, 1)
 
-    def dice_detect(self):
+    def detect_dice(self):
         """ When dice_detect task is called. """
+        """ calls DiceDetector module and creates an instance """
+        """ then gets the coordinates of the dice in the pool """
+        """ putting coordinates in dice_coordinates """
+        """ sends coordinates to navigation module """
 
-        print("dice_detect")
-        self.coordinates.append(randint(-1,1))
-        self.coordinates.append(randint(-1,1))
-        self.navigation.nagivate(1, 1, 1, 1)
+        print("detect_dice")
+        detectdice = DiceDetector()
+        #self.coordinates.append(randint(-1,1))
+        #self.coordinates.append(randint(-1,1))
+        dice_coordinates = detectdice.locate_dice()
+        self.navigation.nagivate(dice_coordinates)
 
-    def roulette_detect(self):
+    def detect_roulette(self):
         """ When roulette_detect task is called. """
 
-        print("roulette_detect")
+        print("detect_roulette")
         self.coordinates.append(randint(-1,1))
         self.coordinates.append(randint(-1,1))
         self.navigation.nagivate(1, 1, 1, 1)
 
-    def cash_in_detect(self):
+    def detect_cash_in(self):
         """ When cash_in_detect task is called. """
 
-        print("cash_in_detect")
+        print("detect_cash_in")
         self.coordinates.append(randint(-1,1))
         self.coordinates.append(randint(-1,1))
         self.navigation.nagivate(1, 1, 1, 1)
 
-    def buoy_detect(self):
-        """ When buoy_detect task is called. """
+    def detect_buoy(self):
+        """ When detect_buoy task is called. """
+        """ Once called, will retreive coordinates from ROS with subscriber """
+        """ Then will send coordinates to Navigation module """
+        """ Sub will navigate towards listed coordinates """
 
-        print("buoy_detect")
-        self.coordinates.append(randint(-1,1))
-        self.coordinates.append(randint(-1,1))
-        self.navigation.nagivate(1, 1, 1, 1)
+        # included 2 mothods to broadcast coordinates
+        # could broadcast through ROS or be called by the module name
+        print("detect_buoy")
+        detectbuoy = DetectBuoy()
+        buoy_coordinates = detectbuoy.detect()
+        self.navigation.nagivate(buoy_coordinates)
+
+        pub_task = rospy.Publisher('coordinates', Int32MultiArray, queue_size=10)
+        pub_array = Int32MultiArray(data=buoy_coordinates)
+
+        rospy.loginfo('sending coordinates to ROS')
+        pub_tas.publish(pub_array)
+
+        #rospy.init_node('buoy_coordinates', anonymous=True)
+        #rospy.Subscriber('xy_coordinate', Int32MultiArray, directions)
+
+    def direction(data):
+        """ Directions is called upon after rospy.subscriber to obtain value from ROS """
+
+        rospy.loginfo('Receiving coordinates from buoy_coordinates')
+        coordinates = data.data
+        self.navigation.navigate(coordinates)
 
     def brake(self):
         """ When brake task is called by auv.py. """
