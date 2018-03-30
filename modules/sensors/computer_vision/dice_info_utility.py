fcf88b69f2193c7487a2a4832760110195a9de14
OlinAlvarez <olinalvarez@gmail.com>
2018-03-29 20:32:22 -0700

    made changes to the preprocess, now using canny edges instead of color filtering

--- modules/sensors/computer_vision/dice_info_utility.py
+++ modules/sensors/computer_vision/dice_info_utility.py
@@ -8,54 +8,71 @@ import cv2
 import DiceClassifier as DC
 from matplotlib.widgets import Button
 import time
+import glob
+#frame = data_utils.get_random_die()
 
-frame = data_utils.get_random_die()
-clone = frame.copy()
+
 def save_pos(win):
     cv2.imwrite('pos_dice/' + str(time.time()) + 'dice.jpg',win)
+
+
 def save_neg(win):
     cv2.imwrite('neg_images/' + str(time.time()) + 'dice.jpg',win)
 
-classifier = DC.DiceClassifier()
-lab = cv2.cvtColor(frame, cv2.COLOR_RGB2LAB)
 
-l,a,b = cv2.split(lab)
-clahe = cv2.createCLAHE(3.0, (8,8))
-cl = clahe.apply(l)
-limg = cv2.merge((cl,a,b))
+frames = []
 
-final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-gray = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5,5), 0)
-gedges = cv2.Canny(gray,100,200)
-edges = cv2.Canny(blurred,100,200)
+for img in glob.glob(('dice_test_data/*.jpg')):
+    frames.append(cv2.imread(img))
+print len(frames)
 
-gim, gcontours, _ = cv2.findContours(gedges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-plt.imshow(gedges)
-plt.show()
-gboxes = [cv2.boundingRect(c) for c in gcontours]
+for frame in frames:
+    clone = frame.copy()
 
-grois = [b for b in gboxes if b[2] * b[3] > 400 and math.fabs(b[2] - b[3]) < 30]
-for x,y,w,h in grois:
-    cv2.rectangle(frame, (x,y),(x+w,y+h),utils.colors["green"], 1)
+    classifier = DC.DiceClassifier()
+    lab = cv2.cvtColor(frame, cv2.COLOR_RGB2LAB)
 
-dice = classifier.classify(frame,grois)
+    l,a,b = cv2.split(lab)
+    clahe = cv2.createCLAHE(3.0, (8,8))
+    cl = clahe.apply(l)
+    limg = cv2.merge((cl,a,b))
 
-for x,y,w,h in dice:
-    cv2.rectangle(frame, (x,y),(x+w,y+h),utils.colors["magenta"], 2)
-plt.imshow(frame)
-plt.show()
-print len(dice)
+    final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+    gray = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5,5), 0)
+    gedges = cv2.Canny(gray,100,200)
+    edges = cv2.Canny(blurred,100,200)
 
-for x,y,w,h in dice:
-    portion = clone[y:y+h,x:x+w]
-    plt.imshow(portion)
+    gim, gcontours, _ = cv2.findContours(gedges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    gboxes = [cv2.boundingRect(c) for c in gcontours]
+
+    grois = [b for b in gboxes if b[2] * b[3] > 400 and math.fabs(b[2] - b[3]) < 30]
+
+    '''
+    for x,y,w,h in grois:
+        cv2.rectangle(frame, (x,y),(x+w,y+h),utils.colors["green"], 1)
+    '''
+    dice = classifier.classify(frame,grois)
+
+    for x,y,w,h in dice:
+        cv2.rectangle(frame, (x,y),(x+w,y+h),utils.colors["magenta"], 2)
+    plt.imshow(frame)
     plt.show()
 
-    choice = raw_input('is this a false positive? [y/nn]')
+    print len(dice)
 
-    if(choice == 'y'):
-        save_neg(portion)
+    for x,y,w,h in dice:
+        portion = clone[y:y+h,x:x+w]
+        plt.imshow(portion)
+        plt.show()
 
-    if(choice == 'n'):
-        save_pos(portion)
+        choice = raw_input('is this a false positive? [y/n]')
+
+        if choice == 'y':
+            save_neg(portion)
+
+        if choice == 'n':
+            save_pos(portion)
+
+        if choice == 's':
+            break
