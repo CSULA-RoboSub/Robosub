f27628a1a8e3693a8a5cc4dfeaf702064fc4bfeb
Danny Padilla <dpadilla010@gmail.com>
2018-04-11 16:48:54 -0700

    fix contour filter

--- modules/sensors/computer_vision/GatePreprocessor.py
+++ modules/sensors/computer_vision/GatePreprocessor.py
@@ -8,29 +8,22 @@ class GatePreprocessor:
     def __init__(self):
         self.lower = [0, 180, 55] # would be nice to adjust in cli
         self.upper = [130, 255, 255] # same...
+        self.min_cont_size = 20
+        self.max_cont_size = 200
         self.roi_size = 400
 
 
     def preprocess(self,  img):
         lower = np.array(self.lower, dtype='uint8')
         upper = np.array(self.upper, dtype='uint8')
-        mask = cv2.inRange(img,  lower,  upper)
-        output = cv2.bitwise_and(img,  img,  mask=mask)
+        mask = cv2.inRange(img, lower, upper)
+        output = cv2.bitwise_and(img, img, mask=mask)
         return output, mask
 
 
-    # takes in a list of contours and filters based on upper and lower bound values (length) and returns an np.array
-    def filter_contours(img_contours, lower=20, upper=200):
-        new_contour_list = []
-        for cont in img_contours:
-            if len(cont) > lower and len(cont) < upper:
-                new_contour_list.append(cont)
-        return np.array(new_contour_list)
-
-
-    def get_interest_regions(self,frame):
+    def get_interest_regions(self, frame):
         height, width, lines = frame.shape
-        center = (width / 2, height / 2)
+        #center = (width / 2, height / 2) # not used
         pimage, mask = self.preprocess(frame)
         
         imgray = cv2.cvtColor(pimage, cv2.COLOR_BGR2GRAY)
@@ -41,8 +34,15 @@ class GatePreprocessor:
         
         im, contours, ret = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        filtered_contours = self.filter_contours(contours)
-        
+        # filter the contours based on size
+        #print("contours length (before filtering):", len(contours) )
+        new_contours_list = []
+        for cont in contours:
+            if ( (len(cont) > self.min_cont_size) and (len(cont) < self.max_cont_size) ):
+                new_contours_list.append(cont)
+        filtered_contours = np.array(new_contours_list)
+        #print("contours length (AFTER filtering):", len(filtered_contours) )
+
         boxes = [cv2.boundingRect(c) for c in filtered_contours]
         
         interest_regions = [b for b in boxes if b[2]*b[3] > self.roi_size]
