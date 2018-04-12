b8220e436a0ef87895b28c215f0064048ff4a84d
Danny Padilla <dpadilla010@gmail.com>
2018-04-11 18:04:49 -0700

    add svm to init in constructor

--- modules/sensors/computer_vision/GateClassifier.py
+++ modules/sensors/computer_vision/GateClassifier.py
@@ -10,6 +10,8 @@ import Classifier
 class GateClassifier:
 
     def __init__(self):
+        self.positive_image_path = 'data/gate/positive/*.jpg' # maybe add different file formats??
+        self.negative_image_path = 'data/gate/negative/*.jpg'
         self.min_dim = 80
         self.block_size = (16, 16)
         self.block_stride = (8, 8)
@@ -23,11 +25,8 @@ class GateClassifier:
             self.cell_size,
             self.bins
         )
-        self.lsvm = None # inits in first-time method call
-        
-        self.positive_image_path = "data/gate/positive/*.jpg" # maybe add different file formats??
-        self.negative_image_path = "data/gate/negative/*.jpg"
-            
+        self.lsvm = SVC(kernel="linear", C = 1.0, probability=True, random_state=2)
+        self.train_lsvm()
 
     def get_features_with_label(self, img_data, label):
         data = []
@@ -36,52 +35,51 @@ class GateClassifier:
             feat = self.hog.compute(img[:, :, :3] )
             data.append((feat, label))
         return data
-
-    def get_lsvm(self):
-        if (self.lsvm == None):
-            pos_imgs = []
-            neg_imgs = []
-            for img in glob.glob(self.positive_image_path):
-                n = cv2.imread(img)
-                pos_imgs.append(n)
-            for img in glob.glob(self.negative_image_path):
-                n = cv2.imread(img)
-                neg_imgs.append(n)
-            positive_data = self.get_features_with_label(pos_imgs, 1)
-            negative_data = self.get_features_with_label(neg_imgs, 0)
-
-            data = positive_data + negative_data
-
-            np.random.shuffle(data) # use np instead
-
-            feat, labels = map(list, zip(*data) )
-            feat_flat = [x.flatten() for x in feat]
-
-            features_df = pd.DataFrame(feat_flat)
-            labels_df = pd.Series(labels)
-
-            feat_train, feat_test, label_train, label_test = train_test_split(
-                features_df,
-                labels_df,
-                test_size=0.3,
-                random_state=2
-            )
-
-            self.lsvm = SVC(kernel="linear", C = 1.0, probability=True, random_state=2)
-            self.lsvm.fit(feat_train, label_train)
-            
-            #result = self.lsvm.predict(feat_test) # not used
-
-        return self.lsvm
-
     
+
+    def train_lsvm(self):
+        pos_imgs = []
+        neg_imgs = []
+        
+        for img in glob.glob(self.positive_image_path):
+            n = cv2.imread(img)
+            pos_imgs.append(n)
+            
+        for img in glob.glob(self.negative_image_path):
+            n = cv2.imread(img)
+            neg_imgs.append(n)
+        
+        positive_data = self.get_features_with_label(pos_imgs, 1)
+        negative_data = self.get_features_with_label(neg_imgs, 0)
+        
+        data = positive_data + negative_data
+        
+        np.random.shuffle(data) # use np instead
+        
+        feat, labels = map(list, zip(*data) )
+        feat_flat = [x.flatten() for x in feat]
+        
+        features_df = pd.DataFrame(feat_flat)
+        labels_df = pd.Series(labels)
+        
+        feat_train, feat_test, label_train, label_test = train_test_split(
+            features_df,
+            labels_df,
+            test_size=0.3,
+            random_state=2
+        )
+        self.lsvm.fit(feat_train, label_train)
+
+
     '''
-        this returns the max value for the GATE, (x,y) as topleft corner
-        then w,h and width and height respectively.
+    this returns the max value for the GATE, (x,y) as topleft corner
+    then w,h and width and height respectively.
     '''
     def classify(self, frame, roi): #roi = regions of interest
         gate = None
         max_val = 0
+        print("roi", roi)
+        print("lsvm", self.lsvm)
         for box in roi:
             x, y, w, h = box
             window = frame[y:y + h, x:x + w, :]
@@ -90,4 +88,5 @@ class GateClassifier:
             prob = self.lsvm.predict_proba( feat.reshape(1, -1) )[0]
             if prob[1] > .1 and prob[1] > max_val:
                 gate = box
+        print("classify", gate)
         return gate
